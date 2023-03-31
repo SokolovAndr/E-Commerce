70fd196489da872629ffd15d086612206dcdbaac
SokolovAndr <korch45@yandex.ru>
2023-03-31 19:47:59 +0300

    Initial commit!!

---
+++ app/src/main/java/com/example/e_commerce/MainActivity.java
@@ -0,0 +1,64 @@
+package com.example.e_commerce;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.os.Bundle;
+
+import com.example.e_commerce.adapter.CategoryAdapter;
+import com.example.e_commerce.adapter.CourseAdapter;
+import com.example.e_commerce.model.Category;
+import com.example.e_commerce.model.Course;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView categoryRecycler, courseRecycler;
+    CategoryAdapter categoryAdapter;
+    CourseAdapter courseAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        List<Category> categoryList = new ArrayList<>();
+        categoryList.add(new Category(1, "Игры"));
+        categoryList.add(new Category(2, "Сайты"));
+        categoryList.add(new Category(3, "Языки"));
+        categoryList.add(new Category(4, "Прочее"));
+        
+        setCategoryRecycler(categoryList);
+
+        List<Course> courseList = new ArrayList<>();
+        courseList.add(new Course(1, "course1", "Профессия Java\nразработчик", "1 января","начальный", "#424345"));
+        courseList.add(new Course(2, "python", "Профессия Python\nразработчик","10 января","средний", "#9FA52D"));
+        courseList.add(new Course(3, "travel_car", "Мой\nтекст","15 февраля","продвинутый", "#9FA52D"));
+        courseList.add(new Course(4, "travel_car", "Мой\nтекст2","20 марта","мастер", "#9FA52D"));
+        courseList.add(new Course(5, "travel_car", "Мой\nтекст2","25 апреля","бог", "#9FA52D"));
+
+        setCourseRecycler(courseList);
+    }
+
+    private void setCourseRecycler(List<Course> courseList) {
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        courseRecycler = findViewById(R.id.courseRecycler);
+        courseRecycler.setLayoutManager(layoutManager);
+
+        courseAdapter = new CourseAdapter(this,courseList);
+        courseRecycler.setAdapter(courseAdapter);
+    }
+
+    private void setCategoryRecycler(List<Category> categoryList) {
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false);
+        categoryRecycler = findViewById(R.id.categoryRecycler);
+        categoryRecycler.setLayoutManager(layoutManager);
+
+        categoryAdapter = new CategoryAdapter(this,categoryList);
+        categoryRecycler.setAdapter(categoryAdapter);
+    }
+}
---
+++ app/src/main/java/com/example/e_commerce/adapter/CategoryAdapter.java
@@ -0,0 +1,59 @@
+package com.example.e_commerce.adapter;
+
+import android.content.Context;
+import android.icu.text.Transliterator;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.e_commerce.R;
+import com.example.e_commerce.model.Category;
+
+import java.util.List;
+
+public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.CategoryViewHolder>{
+
+    Context context;  //для создания страницы со списком
+    List<Category> categories; //список всех наших категорий
+
+    public CategoryAdapter(Context context, List<Category> categories) //просто конструктор с помощью которого мы установим контекст и лист
+    {
+        this.context = context;
+        this.categories = categories;
+    }
+
+    @NonNull
+    @Override
+    public CategoryViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)  //мы указываем дизайн и с какими элементами мы будем работать
+    {
+        View categoryItems = LayoutInflater.from(context).inflate(R.layout.category_item, parent, false); //для всех наших элементов будет использован один дизайн
+        return new CategoryViewHolder(categoryItems);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull CategoryViewHolder holder, int position) //что конкретно в кажое поле мы будем подставлять
+    {
+        holder.categoryTitle.setText(categories.get(position).getTitle()); //holder - это передаваемый параметр
+    }
+
+    @Override
+    public int getItemCount() {
+        return categories.size();
+    }
+
+    public static final class CategoryViewHolder extends RecyclerView.ViewHolder{
+
+        TextView categoryTitle; //прописали с какими конкретными полями мы будем взаимодействовать
+
+        public CategoryViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            categoryTitle = itemView.findViewById(R.id.categoryTitle); //
+        }
+    }
+
+}
